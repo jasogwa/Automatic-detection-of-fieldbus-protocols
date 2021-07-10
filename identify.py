c72ee540d31fb80d5a09656639e090175543e9fc
Jonas <jonaschinagorom@gmail.com>
2021-07-10 18:28:33 +0200

    identify sequence

---
+++ identify.py
@@ -0,0 +1,18 @@
+import Levenshtein
+from query.query import *  
+
+test = "BCE0362E094C000080"
+db = QUERY()
+
+#score two sequences
+def get_score(seq1, seq2):
+    score = Levenshtein.ratio(seq1.replace('-',''),seq2.replace('-','')) * 100
+    return score
+
+rows = db.select()
+
+for row in rows:
+    seqA = row[2]
+    score = get_score(seqA,test)
+    if score >= 60:
+        print(row[1])
