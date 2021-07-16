7ccbb1374b2f7f8071a0d0a17d71bd3369c1f39d
Jonas <jonaschinagorom@gmail.com>
2021-07-16 15:15:29 +0200

    identify message

--- identify.py
+++ identify.py
@@ -2,6 +2,7 @@ import Levenshtein
 from query.query import *  
 
 test = "BCE0362E094C000080"
+#test = "BCE0FFFFFFFFFFFFFF"
 db = QUERY()
 
 #score two sequences
@@ -11,10 +12,17 @@ def get_score(seq1, seq2):
 
 rows = db.select()
 
+result = []
+
 for row in rows:
     seqA = row[2]
-    score = get_score(seqA,test)
+    score = get_score(seqA,test)    
     if score >= 60:
-        print(row[1])
+        print("{}\n{}{}".format(row[1],"Score: ",score))
     else:
-        print("No match was found!")
+        result.append("No sequence found")
+
+if(len(result) != 0):
+    print(result[0])
+else:
+    print("")
