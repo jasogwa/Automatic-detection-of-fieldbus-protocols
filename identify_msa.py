ce1c7d2b29801bbaf9927e99dc2ed0fdaaa8cd45
jon81185 <jonas.asogwa@cml.fraunhofer.de>
2021-08-24 14:36:47 +0200

    identify using msa

---
+++ identify_msa.py
@@ -0,0 +1,28 @@
+import Levenshtein
+from query.query import *  
+
+test = "BCE0362E094C000080"
+#test = "BCE0FFFFFFFFFFFFFF"
+db = QUERY()
+
+#score two sequences
+def get_score(seq1, seq2):
+    score = Levenshtein.ratio(seq1.replace('-',''),seq2.replace('-','')) * 100
+    return score
+
+rows = db.selectMsa()
+
+result = []
+
+for row in rows:
+    seqA = row[2]
+    score = get_score(seqA,test)    
+    if score >= 60:
+        print("{} {}{}\n".format(row[2]," Similarity Score: ",score))
+    else:
+        result.append("No sequence found")
+
+if(len(result) != 0):
+    print(result[0])
+else:
+    print("")
